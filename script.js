6a5be5bfc8d5210745c0f9a22683b0ad3bff940d
Ban_1 <93013896+Udoekaobe1@users.noreply.github.com>
2024-05-28 16:23:25 +0100

    new

--- script.js
+++ script.js
@@ -30,4 +30,14 @@ document.querySelectorAll('button.decrement').forEach(button => {
     });
 });
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
